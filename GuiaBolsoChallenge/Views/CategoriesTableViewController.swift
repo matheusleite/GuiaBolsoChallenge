c455c30b4377a553605d1aba1543f88b22620a8d
Matheus Leite <matheus.leite27@gmail.com>
2019-06-28 00:19:00 -0300

    Add loading indicator and display alert error on fail

--- GuiaBolsoChallenge/Views/CategoriesTableViewController.swift
+++ GuiaBolsoChallenge/Views/CategoriesTableViewController.swift
@@ -10,35 +10,54 @@ import UIKit
 
 class CategoriesTableViewController: UITableViewController {
 
-    private let categoriesViewModel = CategoriesViewModel()
     private let cellIdentifier = "categoriesCell"
+    private let categoriesViewModel = CategoriesViewModel()
+    private let activityIndicator = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.configLoading()
         self.categoriesViewModel.updatedList = {
             DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
                 self.tableView.reloadData()
             }
         }
         
         //TODO: Show a alert indicate error
         self.categoriesViewModel.hasError = {
-            
+            DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
+                self.showAlertError()
+            }
         }
         
         tableView.rowHeight = 60
         tableView.register(CategoriesTableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
     }
 
+    func configLoading() {
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.color = UIColor.black
+        tableView.backgroundView = activityIndicator
+        self.activityIndicator.startAnimating()
+    }
+    
+    func showAlertError() {
+        let alert = UIAlertController(title: "Sorry :(", message: "We can't load categories from server.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return self.categoriesViewModel.numberOfRows()
     }
 
